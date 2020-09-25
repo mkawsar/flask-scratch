0e987dff925e95bdb5232bddbeda7f173f8c4e75
mkawsar <kawsar.diu888@gmail.com>
2020-09-26 01:38:18 +0600

    Added public id

--- app/main/util/dto.py
+++ app/main/util/dto.py
@@ -6,5 +6,6 @@ class UserDo:
     user = api.model('user', {
         'email': fields.String(required=True, description='user email address'),
         'username': fields.String(required=True, description='user username'),
-        'password': fields.String(required=True, description='user password')
+        'password': fields.String(required=True, description='user password'),
+        'public_id': fields.String(description='user Identifier')
     })
